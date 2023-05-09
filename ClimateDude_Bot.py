035dfe418f84a46d56fe46e4848ae264e71c6f94
Shiva-Menon-97 <30766602+Shiva-Menon-97@users.noreply.github.com>
2023-05-09 12:31:52 +0530

    Update ClimateDude_Bot.py
    Hopefully the final modifications!

--- ClimateDude_Bot.py
+++ ClimateDude_Bot.py
@@ -72,16 +72,13 @@ user_input = st.text_input("You:")
 
 # Create a session state object to store the chat history
 if "chat_history" not in st.session_state:
-    st.session_state.chat_history = """Hey there! This is IPCCbro speaking. My job is to help clear up any doubts or confusions you might have about \
-Climate Change and other Environmental Issues. 
+    st.session_state.chat_history = """Hey there! This is IPCCbro speaking. My job is to help clear up any doubts or confusions you might have about Climate Change and other Environmental Issues. 
 
-Primarily, I will do this by providing you references to relevant sections of previous IPCC (Inter-Governmental Panel \
-on Climate Change) reports dating upto March 2023, i.e. upto the 6th Assessment Report (AR6).
+Primarily, I will do this by providing you references to relevant sections of previous IPCC (Inter-Governmental Panel on Climate Change) reports dating upto March 2023, i.e. upto the 6th Assessment Report (AR6).
 
 PS: Abhi, if you're reading this - it's okay to tell people you love watching Chota Bheem 
 
-Note - this is an unofficial bot that refers to the IPCC reports. The creator has no connection to the great folks \
-behind these reports.
+Note - this is an unofficial bot that refers to the IPCC reports. The creator has no connection to the great folks behind these reports.
 """
 
 # If the user input is not empty, append it to the chat history and call the chatbot function
