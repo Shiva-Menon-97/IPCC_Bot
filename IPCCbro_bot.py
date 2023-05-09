49218e64b7f20c331c1dbfce09b2d107803e2ca6
Shiva-Menon-97 <30766602+Shiva-Menon-97@users.noreply.github.com>
2023-05-09 22:54:30 +0530

    Update IPCCbro_bot.py

--- IPCCbro_bot.py
+++ IPCCbro_bot.py
@@ -66,7 +66,7 @@ def get_completion_from_messages(messages, model="gpt-3.5-turbo", temperature=0)
                 )
     return responses.choices[0].message["content"]
 
-def collect_messages(prompt)):
+def collect_messages(prompt):
     context.append({'role':'user', 'content':f"{prompt}"})
     response = get_completion_from_messages(context) 
     context.append({'role':'assistant', 'content':f"{response}"})
