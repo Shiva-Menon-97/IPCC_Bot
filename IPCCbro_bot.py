a9c981d99ac53b091c5d9e157bbafd1916a5f33b
Shiva-Menon-97 <30766602+Shiva-Menon-97@users.noreply.github.com>
2023-05-10 08:29:03 +0530

    Update IPCCbro_bot.py

--- IPCCbro_bot.py
+++ IPCCbro_bot.py
@@ -8,6 +8,7 @@ api_key = st.secrets['key']
 
 openai.api_key = api_key
 
+# System message that dictates how the Chatbot behaves
 context = [ {'role':'system', 'content':"""
 You are IPCCbro, a chatbot designed to combat misinformation about Climate Change and \ 
 other Environmental Issues (henceforth collectively referred to as CC). You are friendly and make \
@@ -50,11 +51,6 @@ Chapter Name: Specify which chapter you are referring to while inspecting the Us
 Conclusions drawn from these inputs: Based on what you read from the relevant Assessment Report, explain your conclusions about the User's statement. Prioritize IPCC AR6 report. Limit your explanation to 60 words. Additionally, quote a relevant statement from the report verbatim.
 **
 
-**
-When elaborating on your conclusions, Always Always Mention At Least One Piece Of Statistical/Numerical Information!!!
-Also - Always Always Ensure that each sentence is on a completely new line!!!
-**
-
 When you are in Situation 1, always structure your answer in the above mentioned fashion.
 
 Situation 2:
@@ -69,6 +65,7 @@ is related to CC. If the claim isn't related, then please issue a statement tell
 topic you're interested in talking about is CC. Make your response humorous!
 """}]  # accumulate messages
 
+# The starting message that's visible at the start of every new chat
 start_msg ="""
 Hey there! This is IPCCbro speaking. I help clear up your doubts or confusions about Climate Change and other Environmental Issues \
 by providing you references to relevant sections of the IPCC reports.
@@ -82,7 +79,7 @@ PS: Abhi, if you're reading this - it's okay to tell people you love watching Ch
 Note - this is an unofficial bot. The creator has no connection to the great folks behind the IPCC reports.
 """
 
-
+# Function to produce a response from OpenAI's API
 def get_completion_from_messages(messages, model="gpt-3.5-turbo", temperature=0):
     responses = openai.ChatCompletion.create(
                 messages=messages,
@@ -91,6 +88,7 @@ def get_completion_from_messages(messages, model="gpt-3.5-turbo", temperature=0)
                 )
     return responses.choices[0].message["content"]
 
+# Function to accumulate the messages to expand context
 def collect_messages(prompt):
     context.append({'role':'user', 'content':f"{prompt}"})
     response = get_completion_from_messages(context, temperature=0) 
@@ -101,6 +99,8 @@ def collect_messages(prompt):
 st.title("The IPCCbro Chatbot :)")
 
 # Storing the chat
+# At the start of a new chat session, I display the start_msg; this happens only once,
+# and then later as the chat progresses, this message is cleared from the 'generated' list.
 if 'generated' not in st.session_state:
     st.session_state['generated'] = []
     st.session_state.generated.append(start_msg)
@@ -113,12 +113,14 @@ if 'past' not in st.session_state:
 # Create a text input widget for the user input
 user_input = st.text_input("You: ",key="input")
 
+# Accumulating both the User's and Chatbot's responses
 if user_input:
     output = collect_messages(user_input)
     # store the output 
     st.session_state.past.append(user_input)
     st.session_state.generated.append(output)
 
+# After the messages have been updated, displaying the results
 if st.session_state['generated']:
     for i in range(len(st.session_state['generated'])-1, -1, -1):
         message(st.session_state["generated"][i], key=str(i))
